6a0e6d46ef95af5fccd069f10efd9683fb95069a
Sungwoo Kahng <lexkahng@gmail.com>
2018-07-06 02:17:34 -0700

    uploading routes.sh

--- routes.sh
+++ routes.sh
@@ -1,7 +1,33 @@
 #!/bin/bash
+####################################################################################
+#The script will be used to retrieve network resource relationships to the instance. 
+#The script currently takes instanceID as input.
+#
+####################################################################################
+read -p "Enter that instanceID: " instanceID
 
-echo Enter that instanceID:
 
-read instanceID
+# instance type, running state, subnet, az
+instanceType=`aws ec2 describe-instance-attribute --instance-id $instanceID --attribute instanceType | sed -n '2p' | awk '{print $2}'`
+currentStatus=`aws ec2 describe-instances --instance-ids $instanceID --query 'Reservations[*].Instances[*].[State.Name]' --output text`
+subnetId=`aws ec2 describe-instances --instance-ids $instanceID --query 'Reservations[*].Instances[*].[SubnetId]' --output text`
+availabilityZone=`aws ec2 describe-instances --instance-ids $instanceID --query 'Reservations[*].Instances[*].[SubnetId]' --output text`
+routeTable=`aws ec2 describe-route-tables --query "RouteTables[*].Associations[?SubnetId=='$subnetId'].RouteTableId" --output text`
 
-echo I will check for routes of "$instanceID"
+echo "Instance type is:	$instanceType"
+echo "It is currently $currentStatus"
+echo "The subnet is $subnetId"
+
+if [[ -z "$routeTable" ]]; then
+	echo The instance is using the main route table.
+else
+	echo "$routeTable" is the routeTableID of the instance.
+fi
+
+#interactive query to find if user wantes this answer.
+read -p "Do you want to find the routes? (y/n) " wantsRoute
+while [[ "$wantsRoute" != "y" && "$wantsRoute" != "Y" && "$wantsRoute" != "n" && "$wantsRoute" != "N" ]]; do
+        read -p "You gotta enter \"y\" or \"n\": " wantsRoute 
+done
+
+echo "end of test."
